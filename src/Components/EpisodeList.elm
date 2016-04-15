79c131fd2633ab50a51f36789a30afb4d94a5918
Michael Gilliland <mjg.py3@gmail.com>
2016-04-14 23:19:05 -0400

    Made episodes links

--- src/Components/EpisodeList.elm
+++ src/Components/EpisodeList.elm
@@ -1,6 +1,7 @@
 module Components.EpisodeList (model, view, update, Action, Model) where
 
-import Html exposing (div, text, ul, li)
+import Html exposing (div, text, ul, li, a)
+import Html.Attributes exposing (style, target)
 import Html.Events exposing (onClick)
 
 import Data.Episodes exposing (orderedEpisodes)
@@ -16,8 +17,21 @@ view address model =
   div []
     [
       ul []
-        <| List.map (\x -> li [] [text <| properTitle x]) orderedEpisodes
+        <| List.map (\episode -> li [] [episodeLink address episode]) orderedEpisodes
     ]
+
+episodeLink : Signal.Address Action -> Episode -> Html.Html
+episodeLink address episode =
+  a
+    [target "_self", style linkStyle, onClick address (SelectEpisode episode)]
+    [text <| properTitle episode]
+
+linkStyle : List (String, String)
+linkStyle = [
+    ("text-decoration", "underline")
+  , ("color", "blue")
+  , ("cursor", "pointer")
+  ]
 
 type Action = SelectEpisode Episode
 

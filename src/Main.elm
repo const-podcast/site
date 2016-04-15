16f3076a3e0dc7c94a9e28b4b4c8ebd2dd6d31e8
Michael Gilliland <mjg.py3@gmail.com>
2016-04-14 22:23:37 -0400

    Main page is looking better. Remove generated files from git.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main where
 
-import Html exposing (div, button, text)
+import Html exposing (div, button, text, h1, ul, li)
 import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
 import Data.Episodes exposing (orderedEpisodes)
@@ -8,20 +8,19 @@ import Data.Episodes exposing (orderedEpisodes)
 main =
   StartApp.start { model = model, view = view, update = update }
 
-
 model = 0
-
 
 view address model =
   div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
+    [
+      h1 [] [text "const (the Podcast)"]
+      , ul []
+        <| List.map (li [])
+        <| List.map (\x -> [text x.title]) orderedEpisodes
     ]
 
 
 type Action = Increment | Decrement
-
 
 update action model =
   case action of

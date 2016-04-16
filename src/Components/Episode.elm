c82d7d514df7e71ae855888447d551e4c75beacd
Michael Gilliland <mjg.py3@gmail.com>
2016-04-16 13:01:58 -0400

    Resolves #2. Allow markdown in episode summaries.

--- src/Components/Episode.elm
+++ src/Components/Episode.elm
@@ -1,6 +1,7 @@
 module Components.Episode (view) where
 
 import Html exposing (h2, div, text)
+import Markdown
 
 import Models.Episode exposing (Episode)
 
@@ -11,4 +12,5 @@ view model =
   div []
     [
       h2 [] [text model.title]
+    , Markdown.toHtml model.summary
     ]
--- src/Data/Episodes/CodeSounds.elm
+++ src/Data/Episodes/CodeSounds.elm
@@ -6,5 +6,16 @@ episode : Episode
 episode = {
     title = "Code Sounds"
   , guests = ["Ethan Winograd"]
-  , summary = "TODO.."
+  , summary = summary
   }
+
+summary : String
+summary = """
+# Test
+
+This is a test of the markdown capabilities.
+ - Thing
+ - Other thing
+ - [github](https://www.github.com)
+
+"""

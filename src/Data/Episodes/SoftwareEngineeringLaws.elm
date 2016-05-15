72df8e98801997e1129b421f3144ec6af77acb9e
Michael Gilliland <mjg.py3@gmail.com>
2016-05-14 21:50:28 -0400

    Resolve #5. Show notes for first 3 episodes.

--- src/Data/Episodes/SoftwareEngineeringLaws.elm
+++ src/Data/Episodes/SoftwareEngineeringLaws.elm
@@ -18,4 +18,23 @@ summary : String
 summary = """
 There are many so-called engineering "laws". Some seem true, some
 ridiculous. Join us as we discuss some of these more popular laws.
+
+#### Show Notes
+ - [Phil Haack's list we followed](http://haacked.com/archive/2007/07/17/the-eponymous-laws-of-software-development.aspx/)
+ - [Postel's Law](https://en.wikipedia.org/wiki/Robustness_principle)
+ - [Hypermedia](https://en.wikipedia.org/wiki/Hypermedia)
+ - [Function Overloading](https://msdn.microsoft.com/en-us/library/5dhe1hce.aspx)
+ - [Parkinson's Law](https://en.wikipedia.org/wiki/Parkinson%27s_law)
+ - [Kanban](https://en.wikipedia.org/wiki/Kanban)
+ - [Phil Haack](http://haacked.com/about/)
+ - [Pareto Principle](https://en.wikipedia.org/wiki/Pareto_principle)
+ - [Previous episode: the Grind](http://www.constpodcast.com/#!/episode/the-grind)
+ - [Sturgeon's Law](https://en.wikipedia.org/wiki/Sturgeon%27s_law)
+ - [The Peter Principle](https://en.wikipedia.org/wiki/Peter_principle)
+ - [The Dilbert Principle](https://en.wikipedia.org/wiki/Dilbert_principle)
+ - [Hofstadter's Law](https://en.wikipedia.org/wiki/Hofstadter%27s_law)
+ - [Analysis Paralysis](https://en.wikipedia.org/wiki/Analysis_paralysis)
+ - [Murphy's Law](https://en.wikipedia.org/wiki/Murphy%27s_law)
+ - [Brook's Law](https://en.wikipedia.org/wiki/Brooks%E2%80%99_law)
+
 """
